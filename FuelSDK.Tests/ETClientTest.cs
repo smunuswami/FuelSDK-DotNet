e6397bdab19581e0d2c19b41a636abea390fcdcc
Sivakumar Munuswami <SMUNUSWAMI@outlook.com>
2017-04-09 21:37:53 -0400

    Added Unit Test project

---
+++ FuelSDK.Tests/ETClientTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FuelSDK;
+
+namespace FuelSDK.Tests
+{
+    [TestClass]
+    public class ETClientTest
+    {
+        [TestMethod]
+        public void ETClientJWTParameter()
+        {
+
+        }
+    }
+}
