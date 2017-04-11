228f3ea0f7f64a2d9a5d4b584a8586c050250812
Sivakumar Munuswami <SMUNUSWAMI@outlook.com>
2017-04-10 20:47:11 -0400

    Added Unit test cases for negative scenario.

--- FuelSDK.Tests/ETClientTest.cs
+++ FuelSDK.Tests/ETClientTest.cs
@@ -1,16 +1,58 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FuelSDK;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace FuelSDK.Tests
 {
     [TestClass]
     public class ETClientTest
     {
-        [TestMethod]
-        public void ETClientJWTParameter()
-        {
+        private NameValueCollection _parameters;
 
+        [TestInitialize]
+        public void Init()
+        {
+            _parameters = new NameValueCollection();
+            _parameters.Add("clientId","Dummy Id");
+            _parameters.Add("clientSecret", "Its a secret, dont tell anybody");
+            _parameters.Add("appSignature", "Autograph");
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ClientCredentialMissingException))]
+        public void ETClientNoClientId()
+        {
+            //set empty to client id and save the configuration to simulate emtpy client id  in config file
+            _parameters["clientId"] = string.Empty;
+
+            //now construct a ET_Client object
+            var etClient = new ET_Client(_parameters);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ClientCredentialMissingException))]
+        public void ETClientNoClientSecret()
+        {
+            //set empty to client id and save the configuration to simulate emtpy client id  in config file
+            _parameters["clientSecret"] = string.Empty;
+
+            //now construct a ET_Client object
+            var etClient = new ET_Client(_parameters);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AppSignatureMissingException))]
+        public void ETClientNoAppSignature()
+        {
+            //set empty to client id and save the configuration to simulate emtpy client id  in config file
+            _parameters["appSignature"] = string.Empty;
+            _parameters.Add("jwt", "{'item1','value1'}");
+
+            //now construct a ET_Client object
+            var etClient = new ET_Client(_parameters);
         }
     }
 }
